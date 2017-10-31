f99c3eca556dc6bacdc70529fe53642399523859
Yasmin Rodriguez <yasmin.rodriguez@gmail.com>
2017-10-31 16:49:08 -0400

    Completed lab 8

--- src/BattingAverageCalculator.java
+++ src/BattingAverageCalculator.java
@@ -9,22 +9,39 @@ import java.util.Scanner;
  */
 public class BattingAverageCalculator {
 
+	/**
+	 * Asks the number of at-bats, for each bat asks the number of bases earned by
+	 * the batter. Calls methods to calculate the batting average and slugging
+	 * percentage.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		boolean keepGoing = true;
 		System.out.println("Welcome to Batting Average Calculator!");
+		System.out.println();
 
-		System.out.print("Enter number of times at bat: ");
-		final int ARRAY_SIZE = scan.nextInt();
-		int[] atBats = new int[ARRAY_SIZE];
+		while (keepGoing) {
+			final int ARRAY_SIZE = Validator.getInt(scan, "Enter number of times at bat: ", 1, Integer.MAX_VALUE);
+			int[] atBats = new int[ARRAY_SIZE];
+			System.out.println();
 
-		System.out.println("0=out, 1=single, 2=double, 3=triple, 4=home run");
-		for (int i = 0; i < atBats.length; i++) {
-			System.out.print("Result for at-bat " + i + ": ");
-			atBats[i] = scan.nextInt();
+			System.out.println("0=out, 1=single, 2=double, 3=triple, 4=home run");
+			for (int i = 0; i < atBats.length; i++) {
+				System.out.print("Result for at-bat " + i + ": ");
+				atBats[i] = Validator.getInt(scan, "Result for at-bat " + i + ": ", 0, 4);
+			}
+
+			System.out.println();
+
+			System.out.printf("Batting average: %.3f\n", calculateBattingAverage(atBats));
+			System.out.printf("Slugging Percentage: %.3f\n", calculateSlugging(atBats));
+			System.out.println();
+
+			keepGoing = Executor.wantToContinue("Another batter? (y/n): ", scan);
 		}
-
-		System.out.printf("Batting average: %.3f\n", calculateBattingAverage(atBats));
-		System.out.printf("Slugging Percentage: %.3f\n", calculateSlugging(atBats));
+		System.out.println("Goodbye!");
 	}
 
 	/**
